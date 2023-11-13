f6ede7204f4b6e8ecabc4fe9568be5fb6d34aa2a
John Fabian <120048034+jnfabbs@users.noreply.github.com>
2023-11-13 19:30:01 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -3,7 +3,7 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - Printf function
+ * _printf - Printf function.
  * @format: format.
  * Return: Printed chars.
  */
@@ -53,9 +53,9 @@ int _printf(const char *format, ...)
 }
 
 /**
- * print_buffer - Prints the contents of the buffer if it exist
+ * print_buffer - print buffer content if exists
  * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
+ * @buff_ind: Index which add next char, reps the length.
  */
 void print_buffer(char buffer[], int *buff_ind)
 {
