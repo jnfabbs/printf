c0151dd9aaf93aa70fa2b9deb6fec072d735aa22
John Fabian <120048034+jnfabbs@users.noreply.github.com>
2023-11-13 19:55:18 +0300

    Update utils.c

--- utils.c
+++ utils.c
@@ -3,7 +3,6 @@
 /**
  * is_printable - Evaluates if a char is printable
  * @c: Char to be evaluated.
- *
  * Return: 1 if c is printable, 0 otherwise
  */
 int is_printable(char c)
@@ -24,7 +23,6 @@ int is_printable(char c)
 int append_hexa_code(char ascii_code, char buffer[], int i)
 {
 	char map_to[] = "0123456789ABCDEF";
-	/* The hexa format code is always 2 digits long */
 	if (ascii_code < 0)
 		ascii_code *= -1;
 
@@ -40,7 +38,6 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 /**
  * is_digit - Verifies if a char is a digit
  * @c: Char to be evaluated
- *
  * Return: 1 if c is a digit, 0 otherwise
  */
 int is_digit(char c)
@@ -55,7 +52,6 @@ int is_digit(char c)
  * convert_size_number - Casts a number to the specified size
  * @num: Number to be casted.
  * @size: Number indicating the type to be casted.
- *
  * Return: Casted value of num
  */
 long int convert_size_number(long int num, int size)
@@ -72,7 +68,6 @@ long int convert_size_number(long int num, int size)
  * convert_size_unsgnd - Casts a number to the specified size
  * @num: Number to be casted
  * @size: Number indicating the type to be casted
- *
  * Return: Casted value of num
  */
 long int convert_size_unsgnd(unsigned long int num, int size)
